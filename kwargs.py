a30bd90f893497ca95442f9e18d95250af00e9c1
Jose <jose.gomezcant@gmail.com>
2023-07-06 22:27:59 -0500

    Archivo kwargs.py añadido. Microtutorial: Cómo funcionan los kwargs

---
+++ kwargs.py
@@ -0,0 +1,28 @@
+# Sígueme para más videos! ↓ ↓ ↓
+# Youtube:   @naufragotech
+# Instagram: @naufragotech
+# Tik Tok:   @naufragotech
+
+# *** Cómo funcionan los **kwargs ***
+def funcion_kwargs(**kwargs):
+    almuerzo = "Hoy almorcé: \n"
+
+    if "entrada" in kwargs:
+        almuerzo += "De entrada: " \
+                    f"{kwargs['entrada']}\n"
+
+    if "segundo" in kwargs:
+        almuerzo += "De segundo: " \
+                    f"{kwargs['segundo']}\n"
+
+    if "postre" in kwargs:
+        almuerzo += "Y de postre: " \
+                    f"{kwargs['postre']}"
+
+    print(almuerzo)
+
+
+funcion_kwargs(
+    entrada="Papa a la huancaína",
+    segundo="Tallarines verdes",
+)

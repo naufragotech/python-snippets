6458860455b74ca6888e0e31e798accadeccecb8
Jose <jose.gomezcant@gmail.com>
2023-07-11 22:10:14 -0500

    Archivo for-pt1.py agregado. Microtutorial serie de cómo usar for en Python - Parte 1

---
+++ for-pt1.py
@@ -0,0 +1,9 @@
+# Sígueme para más videos! ↓ ↓ ↓
+# Youtube:   @naufragotech
+# Instagram: @naufragotech
+# Tik Tok:   @naufragotech
+
+# *** Cómo usar for en Python - Pt 1 ***
+
+for i in range(10):
+    print(i ** 2, end=" ")

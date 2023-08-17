0c019266153d03c021fa377334fd23231974e3a5
Jose <jose.gomezcant@gmail.com>
2023-08-17 16:47:45 -0500

    Microtutorial max agregado

---
+++ funciones-built-in/max.py
@@ -0,0 +1,13 @@
+# Sígueme para más videos! ↓ ↓ ↓
+# Youtube:   @naufragotech
+# Instagram: @naufragotech
+# Tik Tok:   @naufragotech
+
+# *** Cómo usar la función max ***
+
+# l1 = [1, 2, 3, 4, 5]
+#
+# print(max(l1))
+
+l2 = [1, 2, 1, 3, 1, 5, 1, 2]
+print(max(l2, key=lambda x: l2.count(x)))
